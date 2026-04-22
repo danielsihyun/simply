a19c7cca3d0a9d827939747194e64ade3d3cd454
danielsihyun <dl4457@nyu.edu>
2026-04-21 22:45:21 -0400

    quick text debug 8

--- simply/HomeView.swift
+++ simply/HomeView.swift
@@ -682,7 +682,7 @@ struct HomeView: View {
                                     .cornerRadius(4)
                             }
                         } else {
-                            Text(customStep == .calories ? "cal test 3" : "g")
+                            Text(customStep == .calories ? "cal test 4" : "g")
                                 .font(.system(size: 11))
                                 .foregroundColor(.textMuted)
                         }
