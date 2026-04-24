c6685d52030e5b0bbc48ac5f61526e7cbbae6bad
danielsihyun <dl4457@nyu.edu>
2026-04-23 20:16:11 -0400

    placeholder commit 6

--- simply/HomeView.swift
+++ simply/HomeView.swift
@@ -682,7 +682,7 @@ struct HomeView: View {
                                     .cornerRadius(4)
                             }
                         } else {
-                            Text(customStep == .calories ? "cal1" : "g")
+                            Text(customStep == .calories ? "cal2" : "g")
                                 .font(.system(size: 11))
                                 .foregroundColor(.textMuted)
                         }
